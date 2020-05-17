e210d640189d4a0892e267c13d70d97378ab1f59
Dushan Maduranga <dush88gs@gmail.comgit config --global user.email dush88gs@gmail.com>
2020-05-17 13:52:56 +0530

    add cf7 form template code

--- page-contact.php
+++ page-contact.php
@@ -20,6 +20,48 @@ get_header();
 
         <?php echo do_shortcode( '[contact-form-7 id="41" title="Contact Form"]' ); ?>
 
+        <!-- <div class="row">
+          <div class="col-sm-6">
+            <div class="form-group">
+              <label for="contact-name">Name <span class="contact-required">*</span></label>
+              [text* your-name id:contact-name class:form-control placeholder "Enter your name"]
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="form-group">
+              <label for="contact-email">Email <span class="contact-required">*</span></label>
+              [email* your-email id:contact-email class:form-control placeholder "Enter your email"]
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="form-group">
+              <label for="contact-phone">Phone <span class="contact-required">*</span></label>
+              [text* your-phone id:contact-phone class:form-control placeholder "Enter your phone number"]
+            </div>
+          </div>
+        <div class="col-sm-6">
+            <div class="form-group">
+              <label for="contact-country">Country <span class="contact-required">*</span></label>
+              [select* your-country id:contact-country data:countries class:form-control]
+            </div>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <label for="contact-message">Message <span class="contact-required">*</span></label>
+              [textarea* your-message x4 id:contact-message class:form-control placeholder "Enter your message"]            
+            </div>
+          </div>
+          <div class="col-md-12">
+            <p class="text-muted"><strong><span class="contact-required">*</span></strong> These fields are required.</p>
+          </div>
+          <div class="col-md-12">
+            [submit class:btn class:btn-primary "Send Message"]
+          </div>
+        </div> -->
+
 		    </div>
         <div class="col-md-4">
           <?php get_sidebar(); ?>
